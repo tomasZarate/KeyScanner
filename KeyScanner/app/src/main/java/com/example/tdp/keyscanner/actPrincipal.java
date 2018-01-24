6a1786c6dfb8665644d89ae4c815ae2131ae7d54
tomasZarate <31145950+tomasZarate@users.noreply.github.com>
2018-01-24 14:24:20 -0300

    Boton wifi
    Agregado switch para prender y apagar el wifi desde la aplicacion

--- KeyScanner/app/src/main/java/com/example/tdp/keyscanner/actPrincipal.java
+++ KeyScanner/app/src/main/java/com/example/tdp/keyscanner/actPrincipal.java
@@ -1,13 +1,65 @@
 package com.example.tdp.keyscanner;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class actPrincipal extends AppCompatActivity {
+
+    private Toolbar myToolbar;
+    //Wifi
+    private WifiManager wifiManager;
+    private Switch wifiSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_act_principal);
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiSwitch= findViewById(R.id.wifiSwitch);
+        wifiSwitch.setChecked(wifiManager.isWifiEnabled());
+        wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                wifiManager.setWifiEnabled(!wifiManager.isWifiEnabled());
+                // true if the switch is in the On position
+                wifiSwitch.setChecked(!wifiManager.isWifiEnabled());
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                // User chose the "About" item, show the app settings UI...
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+
+        }
+    }
+
 }
