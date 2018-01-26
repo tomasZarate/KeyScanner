63bc6b7fbebacf4393cf4743674b1c3bb6160e01
tomasZarate <31145950+tomasZarate@users.noreply.github.com>
2018-01-25 22:09:25 -0300

    about
    agregada la seccion "about" (Pero hay que completarla)

---
+++ KeyScanner/app/src/main/java/com/example/tdp/keyscanner/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.example.tdp.keyscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
--- KeyScanner/app/src/main/java/com/example/tdp/keyscanner/MainActivity.java
+++ KeyScanner/app/src/main/java/com/example/tdp/keyscanner/MainActivity.java
@@ -3,16 +3,13 @@ package com.example.tdp.keyscanner;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 
-public class actPrincipal extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     protected Toolbar myToolbar;
     protected MenuItem itemWifi;
@@ -23,23 +20,11 @@ public class actPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_act_principal);
+        setContentView(R.layout.activity_main);
         myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        //wifiSwitch= findViewById(R.id.wifiSwitch);
-        //wifiSwitch= (Switch) menu.findItem(R.id.wifi_switch);
-        /*wifiSwitch.setChecked(wifiManager.isWifiEnabled());
-        wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // do something, the isChecked will be
-                wifiManager.setWifiEnabled(!wifiManager.isWifiEnabled());
-                // true if the switch is in the On position
-                wifiSwitch.setChecked(!wifiManager.isWifiEnabled());
-            }
-        });*/
 
     }
 
@@ -68,6 +53,8 @@ public class actPrincipal extends AppCompatActivity {
                 return true;
             case R.id.action_about:
                 // User chose the "About" item, show the app settings UI...
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
                 return true;
             default:
                 // If we got here, the user's action was not recognized.
