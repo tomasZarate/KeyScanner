78df5bd7e521a547c12a69f5180d0a277b50250e
Facu <gonzalofacu@gmail.com>
2018-07-23 18:31:00 -0300

    Patron
    Crear patron

---
+++ ProtoScan/app/src/main/java/com/tdp/protoscan/CrearPatron.java
@@ -0,0 +1,114 @@
+package com.tdp.protoscan;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.andrognito.patternlockview.PatternLockView;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+
+import java.util.List;
+
+import io.paperdb.Paper;
+
+
+public class CrearPatron extends AppCompatActivity {
+
+        String save_pattern_key = "pattern_code";
+        String final_pattern = "";
+        PatternLockView mPatternLockView;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            Paper.init(this);
+            final String save_pattern = Paper.book().read(save_pattern_key);
+            if(save_pattern != null && !save_pattern.equals("null"))
+            {
+                setContentView(R.layout.activity_principal); //actividad para confirmar el patron
+                mPatternLockView = (PatternLockView)findViewById(R.id.pattern_lock_view);
+                mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
+                    @Override
+                    public void onStarted() {
+
+                    }
+
+                    @Override
+                    public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+                    }
+
+                    @Override
+                    public void onComplete(List<PatternLockView.Dot> pattern) {
+                        final_pattern = PatternLockUtils.patternToString(mPatternLockView,pattern);
+                        if(final_pattern.equals(save_pattern)){
+                            Toast.makeText(CrearPatron.this, "Password Correct!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(CrearPatron.this,ActividadDesbloqueada.class); //Aca iria el cuadro flotante
+                            startActivity(intent);
+
+
+                        }else{ Toast.makeText(CrearPatron.this, "Password Incorrecta!", Toast.LENGTH_SHORT).show();}
+
+
+                    }
+
+                    @Override
+                    public void onCleared() {
+
+                    }
+                });
+            }
+            else
+            {
+
+                setContentView(R.layout.activity_main);
+                mPatternLockView = (PatternLockView)findViewById(R.id.pattern_lock_view);
+                mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
+                    @Override
+                    public void onStarted() {
+
+                    }
+
+                    @Override
+                    public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+                    }
+
+                    @Override
+                    public void onComplete(List<PatternLockView.Dot> pattern) {
+                        final_pattern = PatternLockUtils .patternToString(mPatternLockView,pattern);
+
+                    }
+
+                    @Override
+                    public void onCleared() {
+
+                    }
+                });
+
+
+
+
+                Button btnSetup = (Button)findViewById(R.id.btnSetearPatron);
+                btnSetup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Paper.book().write(save_pattern_key, final_pattern);
+                        Toast.makeText(MainActivity.this, "Save pattern okay!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(CrearPatron.this,Principal.class); //clase de la activity para confirmar patron
+                        startActivity(intent);
+                    }
+                });
+            }
+        }
+    }
+
